b1dbc19071724e87af9832cf4f031f1365876224
jstnknr <143372221+jstnknr@users.noreply.github.com>
2024-08-08 17:32:29 -0400

    Update test.js files

--- test/app.test.js
+++ test/app.test.js
@@ -1,78 +1,131 @@
-const { expect } = require('chai')
-const sinon = require('sinon')
-const request = require('supertest')
-const { JSDOM } = require('jsdom')
-const nock = require('nock')
-const app = require('../app')
-const pokeUtil = require('../util/pokemon')
+const { expect } = require('chai');
+const sinon = require('sinon');
+const request = require('supertest');
+const { JSDOM } = require('jsdom');
+const nock = require('nock');
+const app = require('../app');
+const pokeUtil = require('../util/pokemon');
 
 const POKEMON_DATA = {
   name: 'gengar',
   sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/94.png',
-  types: [ 'ghost', 'poison' ],
+  types: ['ghost', 'poison'],
   height: '4 feet 11 inches',
   weight: '89 pounds'
-}
+};
 
 describe('Server Routes', () => {
   describe('GET / - pokemon form', () => {
-    it('should return 200 status')
+    it('should return 200 status', async () => {
+      await request(app)
+        .get('/')
+        .expect(200);
+    });
+
     it('should return content-type html header', async () => {
       await request(app)
         .get('/')
         .expect(200)
-        .expect('content-type', /html/)
-    })
+        .expect('content-type', /html/);
+    });
+
     it('should render form with action of "/pokemon"', async () => {
       const res = await request(app)
         .get('/')
-        .expect(200)
-      const { window: { document } } = new JSDOM(res.text)
-      expect(document.querySelector('form')).to.exist
-    })
-    it('should return input element with name attribute of "name"')
-  })
+        .expect(200);
+      const { window: { document } } = new JSDOM(res.text);
+      expect(document.querySelector('form')).to.exist;
+      expect(document.querySelector('form').getAttribute('action')).to.equal('/pokemon');
+    });
+
+    it('should return input element with name attribute of "name"', async () => {
+      const res = await request(app)
+        .get('/')
+        .expect(200);
+      const { window: { document } } = new JSDOM(res.text);
+      const inputEl = document.querySelector('input[name="name"]');
+      expect(inputEl).to.exist;
+    });
+  });
+
   describe('GET /pokemon - pokemon info page', () => {
-    let pokeStub
+    let pokeStub;
     before(() => {
-      nock.disableNetConnect()
-      nock.enableNetConnect('127.0.0.1')
-    })
+      nock.disableNetConnect();
+      nock.enableNetConnect('127.0.0.1');
+    });
+
     beforeEach(() => {
-      pokeStub = sinon.stub(pokeUtil, 'getPokemon').resolves(POKEMON_DATA)
-    })
-    afterEach(sinon.restore)
+      pokeStub = sinon.stub(pokeUtil, 'getPokemon').resolves(POKEMON_DATA);
+    });
+
+    afterEach(sinon.restore);
     after(() => {
-      nock.cleanAll()
-      nock.enableNetConnect()
-    })
+      nock.cleanAll();
+      nock.enableNetConnect();
+    });
+
     it('should redirect to / if name query param not included', async () => {
       await request(app)
         .get('/pokemon')
         .expect(302)
-        .expect('location', '/')
-    })
+        .expect('location', '/');
+    });
+
     it('/pokemon?name=pokemonName should render pokemon name', async () => {
       const res = await request(app)
         .get('/pokemon?name=banana')
-        .expect(200)
-      expect(pokeStub.calledWith('banana'))
-      const { window: { document } } = new JSDOM(res.text)
-      const nameEl = document.querySelector('[data-test-id="pokemon-name"]')
-      expect(nameEl.textContent).to.include(POKEMON_DATA.name)
-    })
-    it('/pokemon?name=pokemonName should render pokemon image')
-    it('/pokemon?name=pokemonName should render pokemon types')
-    it('/pokemon?name=pokemonName should render pokemon height')
-    it('/pokemon?name=pokemonName should render pokemon weight')
-    it('should render "Pokemon not found" if given non-existent pokemon', async () => {
-      pokeStub.restore()
-      sinon.stub(pokeUtil, 'getPokemon').rejects(new Error('oh no'))
+        .expect(200);
+      expect(pokeStub.calledWith('banana')).to.be.true;
+      const { window: { document } } = new JSDOM(res.text);
+      const nameEl = document.querySelector('[data-test-id="pokemon-name"]');
+      expect(nameEl.textContent).to.include(POKEMON_DATA.name);
+    });
+
+    it('/pokemon?name=pokemonName should render pokemon image', async () => {
       const res = await request(app)
         .get('/pokemon?name=banana')
-        .expect(200)
-      expect(pokeStub.calledWith('banana'))
-      expect(res.text).to.match(/pokemon not found/i)
-    })
-  })
-})
+        .expect(200);
+      const { window: { document } } = new JSDOM(res.text);
+      const imgEl = document.querySelector('img');
+      expect(imgEl).to.exist;
+      expect(imgEl.getAttribute('src')).to.equal(POKEMON_DATA.sprite);
+    });
+
+    it('/pokemon?name=pokemonName should render pokemon types', async () => {
+      const res = await request(app)
+        .get('/pokemon?name=banana')
+        .expect(200);
+      const { window: { document } } = new JSDOM(res.text);
+      const typesEl = document.querySelector('[data-test-id="types"]');
+      expect(typesEl.textContent).to.include(POKEMON_DATA.types.join(', '));
+    });
+
+    it('/pokemon?name=pokemonName should render pokemon height', async () => {
+      const res = await request(app)
+        .get('/pokemon?name=banana')
+        .expect(200);
+      const { window: { document } } = new JSDOM(res.text);
+      const heightEl = document.querySelector('[data-test-id="height"]');
+      expect(heightEl.textContent).to.include(POKEMON_DATA.height);
+    });
+
+    it('/pokemon?name=pokemonName should render pokemon weight', async () => {
+      const res = await request(app)
+        .get('/pokemon?name=banana')
+        .expect(200);
+      const { window: { document } } = new JSDOM(res.text);
+      const weightEl = document.querySelector('[data-test-id="weight"]');
+      expect(weightEl.textContent).to.include(POKEMON_DATA.weight);
+    });
+
+    it('should render "Pokemon not found" if given non-existent pokemon', async () => {
+      pokeStub.restore();
+      sinon.stub(pokeUtil, 'getPokemon').rejects(new Error('oh no'));
+      const res = await request(app)
+        .get('/pokemon?name=banana')
+        .expect(200);
+      expect(res.text).to.match(/pokemon not found/i);
+    });
+  });
+});
--- test/convertUnits.test.js
+++ test/convertUnits.test.js
@@ -1,4 +1,4 @@
-const { expect } = require('chai')
+/* const { expect } = require('chai')
 const {
   getPounds,
   getFeetAndInches
@@ -22,3 +22,46 @@ describe('unit conversion helpers', () => {
     it('should convert 65 decimeters to "21 feet 3 inches"')
   })
 })
+*/
+
+const { expect } = require('chai');
+const {
+  getPounds,
+  getFeetAndInches
+} = require('../util/convertUnits');
+
+describe('unit conversion helpers', () => {
+  describe('getPounds', () => {
+    it('should convert 4600 hectograms to "1014 pounds"', () => {
+      const pounds = getPounds(4600);
+      expect(pounds).to.equal('1014 pounds');
+    });
+
+    it('should convert 400 hectograms to "88 pounds"', () => {
+      const pounds = getPounds(400);
+      expect(pounds).to.equal('88 pounds');
+    });
+
+    it('should convert 560 hectograms to "123 pounds"', () => {
+      const pounds = getPounds(560);
+      expect(pounds).to.equal('123 pounds');
+    });
+  });
+
+  describe('getFeetAndInches', () => {
+    it('should convert 15 decimeters to "4 feet 11 inches"', () => {
+      const height = getFeetAndInches(15);
+      expect(height).to.equal('4 feet 11 inches');
+    });
+
+    it('should convert 21 decimeters to "6 feet 10 inches"', () => {
+      const height = getFeetAndInches(21);
+      expect(height).to.equal('6 feet 10 inches');
+    });
+
+    it('should convert 65 decimeters to "21 feet 3 inches"', () => {
+      const height = getFeetAndInches(65);
+      expect(height).to.equal('21 feet 3 inches');
+    });
+  });
+});
